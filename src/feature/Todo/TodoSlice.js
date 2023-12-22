7124eae4fefda91fbd246c47e634804d0c0a0eee
Arun <arunprajapati8502@gmail.com>
2023-12-22 14:29:52 +0530

    feat:try update feature

--- src/feature/Todo/TodoSlice.js
+++ src/feature/Todo/TodoSlice.js
@@ -17,12 +17,23 @@ export const todoSlice = createSlice({
             }
             state.todos.push(todo)
         },
+        updateTodo: (state, action) => {
+            const { id, newText } = action.payload;
+            
+            // Find the todo to update by its ID
+            const todoToUpdate = state.todos.find((todo) => todo.id === id);
+
+            // If the todo is found, update its text
+            if (todoToUpdate) {
+                todoToUpdate.text = newText;
+            }
+        },
         removeTodo: (state, action) => {
             state.todos = state.todos.filter((todo) => todo.id !== action.payload )
         },
     }
 })
 
-export const { addTodo, removeTodo } = todoSlice.actions
+export const { addTodo, removeTodo,updateTodo } = todoSlice.actions
 
 export default todoSlice.reducer
